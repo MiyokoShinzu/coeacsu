624059839f107412be33dbd0b94378e82250595f
Karl Stephen Evallo <71174588+MiyokoShinzu@users.noreply.github.com>
2025-07-11 16:26:09 +0800

    Update research.php

--- ce/research.php
+++ ce/research.php
@@ -27,6 +27,7 @@
                 <h3 class=" text-start text-white mt-2" style="font-weight: bolder; ">Research</h3>
                 <p class="text-white">Narrative Presentation</p>
             </div>
+<<<<<<< Updated upstream
             <div class="col-lg-6 mx-auto mt-3 p-0 m-0 border border-1">
                 <div class="ratio ratio-16x9 p-0 m-0">
                     <video class="w-100 h-100" style="object-fit: cover;" controls muted autoplay loop>
@@ -34,6 +35,13 @@
                         Your browser does not support the video tag.
                     </video>
                 </div>
+=======
+            <div class="col-lg-6 mx-auto mt-3 d-flex align-items-center justify-content-center image-container">
+                <video width="640" height="360" controls muted autoplay loop>
+                    <source src="../uploads/test.mp4" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+>>>>>>> Stashed changes
 
             </div>
             <div class=" col-lg-12 col-md-12 col-sm-12 d-flex align-items-center justify-start flex-column mx-auto" style="background:rgb(174,14,14)">
@@ -42,7 +50,11 @@
             <div class="col-lg-12 mt-3 mb-3">
 
                 <div class="row w-100 ">
+<<<<<<< Updated upstream
                     <div class="col-lg-10 mx-auto d-flex align-items-center justify-content-center ">
+=======
+                    <div class="col-lg-10 mx-auto d-flex align-items-center justify-content-center">
+>>>>>>> Stashed changes
                         <iframe
                             src="../uploads/NarrativeReport.pdf#toolbar=0&navpanes=0&scrollbar=0"
                             id="n_report"
