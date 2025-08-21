c5f11f23baa8bdfad167d62f3575f812d57f9bb5
Karl Stephen Evallo <71174588+MiyokoShinzu@users.noreply.github.com>
2025-08-21 23:00:06 +0800

    Update connection.php

--- src/connection.php
+++ src/connection.php
@@ -9,7 +9,5 @@ $mysqli = new mysqli($servername, $username, $password, $dbname);
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
-else{
-    echo("connected");
-}
+
 ?>
