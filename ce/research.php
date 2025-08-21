7f484b109b5476d673507de9671af2fc627c7859
Karl Stephen Evallo <71174588+MiyokoShinzu@users.noreply.github.com>
2025-08-21 23:31:41 +0800

    Update research.php

--- ce/research.php
+++ ce/research.php
@@ -24,7 +24,7 @@
         <!-- background: #ff4C01; #FE904D-->
          <?php  $bg = "#f17e0bff"; ?>
         <div class="row d-flex gs-1 mt-1 justify-content-center align-items-center">
-            <div class=" col-lg-11  col-md-12 col-sm-12 d-flex align-items-center justify-start flex-column mx-auto mb-3" style="background:<?php echo $bg; ?>">
+            <div class=" col-lg-12  col-md-12 col-sm-12 d-flex align-items-center justify-start flex-column mx-auto mb-3" style="background:<?php echo $bg; ?>">
                 <h4 class=" text-start text-white mt-2" style="font-weight: bolder; ">Research</h4>
                 <p class="text-white">Narrative Presentation</p>
             </div>
@@ -51,7 +51,7 @@
             }
             ?>
 
-            <div class="mt-3 col-lg-11 col-md-12 col-sm-12 d-flex align-items-center justify-start flex-column mx-auto mb-3" style="background:<?php echo $bg; ?>">
+            <div class="mt-3 col-lg-12 col-md-12 col-sm-12 d-flex align-items-center justify-start flex-column mx-auto mb-3" style="background:<?php echo $bg; ?>">
                 <h3 class="text-white p-3">Narrative Report</h3>
             </div>
             <?php
@@ -72,7 +72,7 @@
             }
             ?>
 
-            <div class=" col-lg-11 col-md-12 col-sm-12 mt-3 mb-5 d-flex align-items-center justify-start flex-column mx-auto" style="background:<?php echo $bg; ?>">
+            <div class=" col-lg-12 col-md-12 col-sm-12 mt-3 mb-5 d-flex align-items-center justify-start flex-column mx-auto" style="background:<?php echo $bg; ?>">
                 <h3 class=" text-start text-white mt-2" style="font-weight: bolder; ">Attachments</h3>
             </div>
             <div class="col-lg-5 mx-auto">
