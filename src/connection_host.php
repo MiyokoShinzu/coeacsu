a78f7e9b1e7817d93ddcf9f69c7713c80fc2a146
Karl Stephen Evallo <71174588+MiyokoShinzu@users.noreply.github.com>
2025-08-21 22:58:19 +0800

    a
    a

--- src/connection_host.php
+++ src/connection_host.php
@@ -9,4 +9,7 @@ $mysqli = new mysqli($servername, $username, $password, $dbname);
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
+else{
+    echo("connected");
+}
 ?>
