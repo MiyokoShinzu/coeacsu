278d556647ab36b6be94619a502e1a2085d0aead
Karl Stephen Evallo <71174588+MiyokoShinzu@users.noreply.github.com>
2025-10-16 10:29:24 +0800

    Update index.php

--- login/index.php
+++ login/index.php
@@ -93,20 +93,20 @@ $mysqli->close();
 </head>
 
 <body>
-    <main class="d-flex w-100 mt-3">
+    <main class="d-flex w-100 mt-1">
 
         <div class="container d-flex flex-column">
 
-            <div class="row mt-5">
+            <div class="row mt-3">
                 <div class="col-11 col-lg-10 text-center mx-auto mt-1">
-                    <img src="../assets/coea_logo.png" class="mt-3 mb-2" alt="" style="height: 100px; width: 100px; margin: auto; display: block;" class="mb-3">
+                    <img src="../assets/coea_logo.png" class="mt-3 mb-2" alt="" style="height: 120px; width: 120px; margin: auto; display: block;" class="mb-3">
 
                     <!-- <img src="https://1000logos.net/wp-content/uploads/2019/03/PNP-Logo.png" alt="" class="mt-3" style="height: 100px; width: 200px;"> -->
 
-                    <h1 class="mt-2 " style="  ">College of Engineering and Architecture</h1>
+                    <h1 class="mt-2 mb-2" style="  ">College of Engineering and Architecture</h1>
 
                 </div>
-                <div class="col-sm-10 col-md-8 col-xl-5 mx-auto d-table mt-3">
+                <div class="col-sm-10 col-md-5 col-xl-4 mx-auto d-table mt-2">
                     <div class="d-table-cell align-middle">
 
                         <div class="text-center ">
