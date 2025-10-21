4db1fc93ac0749e29a357c5e3da3a1d195c288d9
Karl Stephen Evallo <71174588+MiyokoShinzu@users.noreply.github.com>
2025-10-21 16:55:00 +0800

    Update planning.php

--- admin/planning.php
+++ admin/planning.php
@@ -67,6 +67,12 @@
                                     </div>
                                     <div class="col-lg-12 mx-auto mb-3">
                                         <div class="form-group">
+                                            <label class="mb-2" for="">File Label</label>
+                                            <input type="text" name="label" id="label" class="form-control" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12 mx-auto mb-3">
+                                        <div class="form-group">
                                             <label class="mb-2" for="">File Type</label>
                                             <select name="file_type" id="file_type" class="form-select" required>
                                                 <option value="">-- Select File Type --</option>
